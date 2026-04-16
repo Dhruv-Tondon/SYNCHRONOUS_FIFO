df5bdcf77339a69532359fe45940dcaf43ec5409
Dhruv-Tondon <tdhruv350@gmail.com>
2026-04-16 14:00:30 +0530

    implemented pointer's next state logic and flags
    manages flags pointers and logic.

---
+++ CODE/pointers.v
@@ -0,0 +1,45 @@
+module fifo_ctrl #(parameter ADDR_WIDTH = 3)(
+    input wire clk, rst_n, r_en, w_en,
+    output reg full, empty,overflow ,underflow,
+    output wire [ADDR_WIDTH-1:0] waddr, raddr,
+    output wire mem_w_en, mem_r_en
+);
+    reg  [ADDR_WIDTH:0] wr_ptr;
+    reg  [ADDR_WIDTH:0] rd_ptr;
+    
+    wire [ADDR_WIDTH:0] wr_ptr_next, rd_ptr_next;
+    wire full_val, empty_val;
+    wire overflow_val, underflow_val;
+
+    assign mem_w_en = w_en & ~full;
+    assign mem_r_en = r_en & ~empty;
+    assign wr_ptr_next = wr_ptr + mem_w_en; 
+    assign rd_ptr_next = rd_ptr + mem_r_en;
+
+    assign overflow_val = w_en & full;
+    assign underflow_val = r_en & empty;
+    assign empty_val = (rd_ptr_next == wr_ptr_next);
+    assign full_val = (wr_ptr_next[ADDR_WIDTH] != rd_ptr_next[ADDR_WIDTH]) &&
+    (wr_ptr_next[ADDR_WIDTH-1:0] == rd_ptr_next[ADDR_WIDTH-1:0]);
+
+    assign waddr =wr_ptr[ADDR_WIDTH-1:0];
+    assign raddr=rd_ptr[ADDR_WIDTH-1:0];
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            rd_ptr <= 0;
+            wr_ptr <= 0;
+            empty<= 1; 
+            full <= 0;
+            overflow<= 0;
+            underflow<= 0;
+        end else begin
+            rd_ptr <= rd_ptr_next;
+            wr_ptr <= wr_ptr_next;
+            empty <= empty_val;
+            full<= full_val;
+            overflow<= overflow_val;
+            underflow<= underflow_val;
+        end
+    end
+endmodule
